07c0206f4e3fe1ad9eab6d983529832a89be8483
toannguyen-valuence <nguyen.minh.toan@valuence.inc>
2023-11-23 10:47:47 +0700

    feat: config eslint prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,15 +6,34 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
+  settings: {
+    react: { version: '18.2' },
+    'import/resolver': {
+      alias: {
+        map: [
+          ['@components', './src/components'],
+          ['@utils', './src/utils'],
+          // Thêm các alias khác ở đây
+        ],
+        extensions: ['.js', '.jsx', '.json'],
+      },
+    },
+  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    eqeqeq: 'warn', // yêu cầu sử dụng === và !==
+    'no-trailing-spaces': 'warn', // không cho phép dấu cách ở cuối dòng
+    'object-curly-spacing': ['warn', 'always'], // yêu cầu hoặc không cho phép dấu cách bên trong dấu ngoặc nhọn
+    'arrow-spacing': ['warn', { before: true, after: true }], // yêu cầu hoặc không cho phép dấu cách bên trong dấu ngoặc nhọn
+    'no-unused-vars': 'warn', // cảnh báo khi có biến không được sử dụng
+    'no-multiple-empty-lines': ['warn', { max: 1 }], // không cho phép nhiều dòng trống liên tiếp
   },
-}
+};
---
+++ src/components/toan.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const toan = () => {
+  return <div>toan</div>
+}
+
+export default toan
