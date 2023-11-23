740f81ad875b8fdbbd4dde94ac8b8861aa6d2630
Ngo-Tan-Khoa <khoa.ngo-ojt07@devplus.edu.vn>
2023-11-23 16:31:15 +0700

    feat: cài react-router-dom, redux toolkit

--- src/components/toan.jsx
+++ src/components/toan.jsx
@@ -1,8 +1,33 @@
 /* eslint-disable no-unused-vars */
+import { decrement, increment } from '@/redux/slice/counterSlice'
+import { Button, Space } from 'antd'
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
-const toan = () => {
-  return <div>toan</div>
+const Toan = () => {
+  const count = useSelector(state => state.counter.value)
+  const dispatch = useDispatch()
+
+  return (
+    <div>
+      <div>
+        <Button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </Button>
+        <span>{count}</span>
+        <Button
+          danger
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </Button>
+      </div>
+    </div>
+  )
 }
 
-export default toan
+export default Toan
