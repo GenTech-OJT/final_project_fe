f411d930e22ca747978e0110c6780d338fd5f7e4
Ngo-Tan-Khoa <khoa.ngo-ojt07@devplus.edu.vn>
2023-11-23 17:51:30 +0700

    feat: cấu hình router với react-router-dom-v6

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
+/* eslint-disable no-undef */
 import './App.css'
 import Toan from '@/components/Toan'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
+import { routes } from './routes'
+import DefaultComponent from './components/DefaultComponent/DefaultComponent'
+import { Fragment } from 'react'
 function App() {
   return (
     <>
       <Toan />
+      <Router>
+        <Routes>
+          {routes.map((route, index) => {
+            const Page = route.page
+            const Layout = route.isShowHeader ? DefaultComponent : Fragment
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            )
+          })}
+        </Routes>
+      </Router>
     </>
   )
 }
---
+++ src/components/DefaultComponent/DefaultComponent.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/prop-types */
+import React from 'react'
+import HeaderComponent from '../HeaderComponent/HeaderComponent'
+
+const DefaultComponent = ({ children }) => {
+  return (
+    <div>
+      <HeaderComponent />
+      {children}
+    </div>
+  )
+}
+
+export default DefaultComponent
