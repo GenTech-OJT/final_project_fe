69155a42302d4363a84ab0216aaa857ddeb87a3f
kieuvy29 <vy10b3.thptgl@gmail.com>
2023-11-29 17:53:25 +0700

    fix: back to home navigate

--- src/pages/AdminPages/404NotFound/NotFound.jsx
+++ src/pages/AdminPages/404NotFound/NotFound.jsx
@@ -1,9 +1,14 @@
 import { Button } from 'antd'
-import './NotFound.css'
 import { useTranslation } from 'react-i18next'
+import { useNavigate } from 'react-router'
+import './NotFound.css'
 
 const NotFoundPage = () => {
   const { t } = useTranslation('translation')
+  const navigate = useNavigate()
+  const backToHome = () => {
+    navigate('/users')
+  }
   return (
     <div className="background">
       <div className="content">
@@ -11,7 +16,7 @@ const NotFoundPage = () => {
         <p className="content-p">{t('notfound_content')}</p>
         <Button
           type="primary"
-          href="../Dashboard/index.jsx"
+          onClick={backToHome}
           className="back-to-home-btn"
         >
           {t('back_to_home')}
