11ea5f5d65e03d198c908f86eb82e50aa5bc2c79
kieuvy29 <vy10b3.thptgl@gmail.com>
2023-11-29 16:49:09 +0700

    feat: translate notfound page

--- src/pages/AdminPages/404NotFound/NotFound.jsx
+++ src/pages/AdminPages/404NotFound/NotFound.jsx
@@ -1,14 +1,20 @@
 import { Button } from 'antd'
 import './NotFound.css'
+import { useTranslation } from 'react-i18next'
 
 const NotFoundPage = () => {
+  const { t } = useTranslation('translation')
   return (
     <div className="background">
       <div className="content">
         <h1 className="content-h1">404</h1>
-        <p className="content-p">Lost in Space</p>
-        <Button type="primary" className="back-to-home-btn">
-          Back to home
+        <p className="content-p"> {t('lost_in_space')}</p>
+        <Button
+          type="primary"
+          href="../Dashboard/index.jsx"
+          className="back-to-home-btn"
+        >
+          {t('back_to_home')}
         </Button>
       </div>
     </div>
