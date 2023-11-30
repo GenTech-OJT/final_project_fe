69817879db7c33809829fbe02b744f4743b66cda
CaoVinhNguyen2206 <nguyen.cao-ojt07@devplus.edu.vn>
2023-11-30 13:22:50 +0700

    fix: table layout UI fetch api

--- src/pages/AdminPages/EmployeeManagement/index.jsx
+++ src/pages/AdminPages/EmployeeManagement/index.jsx
@@ -1,58 +1,63 @@
 import React, { useState, useEffect, useRef } from 'react'
-import {
-  Table,
-  Space,
-  Input,
-  Typography,
-  Popconfirm,
-  Button,
-  Modal,
-  Form,
-  message,
-} from 'antd'
+import { Table, Button, Modal, Form, Input, Spin, message } from 'antd'
 import {
   SearchOutlined,
-  DeleteOutlined,
   EditOutlined,
-  EyeOutlined,
   SaveOutlined,
   CloseOutlined,
+  EyeOutlined,
+  DeleteOutlined,
 } from '@ant-design/icons'
 import './employeeStyle.css'
 
 const { Search } = Input
-const { Text } = Typography
 
 const EmployeeManagement = () => {
-  const [gridData, setGridData] = useState([
-    {
-      id: 1,
-      name: 'John Doe',
-      status: 'active',
-      position: 'CEO',
-      isManage: 'Yes',
-    },
-    {
-      id: 2,
-      name: 'Adu a Seng',
-      status: 'active',
-      position: 'CEO',
-      isManage: 'No',
-    },
-    // Add more data if needed
-  ])
-
+  const [gridData, setGridData] = useState([])
   const [searchText, setSearchText] = useState('')
   const [sortedInfo, setSortedInfo] = useState({})
   const [pagination, setPagination] = useState({
     current: 1,
-    pageSize: 5,
+    pageSize: 10,
+    total: 0,
   })
-
   const [editRowKey, setEditRowKey] = useState('')
   const [isModalVisible, setIsModalVisible] = useState(false)
+  const [loadingData, setLoadingData] = useState(true)
 
   const formRef = useRef(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:3000/employees?_page=${pagination.current}&_limit=${
+            pagination.pageSize
+          }&_sort=${sortedInfo.columnKey || 'id'}&_order=${
+            sortedInfo.order || 'asc'
+          }&q=${searchText}`
+        )
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`)
+        }
+
+        const apiData = await response.json()
+        console.log(apiData) // In ra giá trị của apiData để kiểm tra
+        setGridData(apiData.data)
+        setPagination({
+          ...pagination,
+          total: apiData.pagination.total,
+        })
+      } catch (error) {
+        console.error('Lỗi khi gọi API:', error)
+      } finally {
+        setLoadingData(false)
+      }
+    }
+
+    fetchData()
+  }, [pagination.current, pagination.pageSize, sortedInfo, searchText])
 
   useEffect(() => {
     if (!isModalVisible && formRef.current) {
@@ -60,63 +65,59 @@ const EmployeeManagement = () => {
     }
   }, [isModalVisible])
 
-  const handleDelete = record => {
-    const updatedData = gridData.filter(item => item.id !== record.id)
-    setGridData(updatedData)
-  }
-
-  const isEditing = record => record.id === editRowKey
-
   const save = async key => {
     try {
       const row = await formRef.current.validateFields()
-      const newData = gridData.map(item =>
-        item.id === key ? { ...item, ...row } : item
-      )
-      setGridData(newData)
+      // Handle the logic to update data if needed
       setEditRowKey('')
     } catch (error) {
-      console.log('Lỗi:', error)
+      console.error('Lỗi khi lưu dữ liệu:', error)
     }
   }
 
   const edit = record => {
-    formRef.current.setFieldsValue({
-      name: '',
-      email: '',
-      age: '',
-      message: '',
-      ...record,
-    })
+    formRef.current.setFieldsValue({ ...record })
     setEditRowKey(record.id)
   }
 
   const cancel = () => setEditRowKey('')
 
+  const viewDetail = record => {
+    // Handle logic to view details for the selected record
+    console.log('View Detail:', record)
+  }
+
+  const deleteRecord = recordId => {
+    // Handle logic to delete the selected record
+    console.log('Delete Record:', recordId)
+  }
+
+  const toggleStatus = record => {
+    // Handle logic to toggle status
+    const updatedGridData = gridData.map(item => {
+      if (item.id === record.id) {
+        return {
+          ...item,
+          status: item.status === 'active' ? 'inactive' : 'active',
+        }
+      }
+      return item
+    })
+    setGridData(updatedGridData)
+    message.success(
+      `${record.status === 'active' ? 'Deactivated' : 'Activated'} successfully`
+    )
+  }
+
   const handleChange = e => {
     const value = e.target.value
     setSearchText(value)
-    setPagination({
-      ...pagination,
-      current: 1,
-    })
+    setPagination({ ...pagination, current: 1 })
   }
 
   const handleTableChange = (pagination, filters, sorter) => {
     setSortedInfo(sorter)
-    setPagination({
-      ...pagination,
-      current: pagination.current,
-    })
-  }
-
-  const handleStatusToggle = record => {
-    const updatedData = gridData.map(item =>
-      item.id === record.id
-        ? { ...item, status: item.status === 'active' ? 'inactive' : 'active' }
-        : item
-    )
-    setGridData(updatedData)
+    setPagination({ ...pagination, current: pagination.current })
   }
 
   const columns = [
@@ -136,13 +137,16 @@ const EmployeeManagement = () => {
     },
     {
       title: 'Status',
+      align: 'center',
       dataIndex: 'status',
       sorter: true,
       sortOrder: sortedInfo.columnKey === 'status' && sortedInfo.order,
-      align: 'center',
-      render: (text, record) => (
-        <Button type="primary" onClick={() => handleStatusToggle(record)}>
-          {text}
+      render: (_, record) => (
+        <Button
+          type={record.status === 'active' ? 'primary' : 'danger'}
+          onClick={() => toggleStatus(record)}
+        >
+          {record.status === 'active' ? 'Active' : 'Inactive'}
         </Button>
       ),
     },
@@ -154,108 +158,76 @@ const EmployeeManagement = () => {
       sortOrder: sortedInfo.columnKey === 'position' && sortedInfo.order,
     },
     {
-      title: 'Is Manage',
+      title: 'Is Manager',
       align: 'center',
-      dataIndex: 'isManage',
+      dataIndex: 'is_manager',
       sorter: true,
-      sortOrder: sortedInfo.columnKey === 'isManage' && sortedInfo.order,
+      sortOrder: sortedInfo.columnKey === 'is_manager' && sortedInfo.order,
     },
     {
       title: 'Action',
       align: 'center',
-      dataIndex: 'action',
-      render: (_, record) => {
-        const editable = isEditing(record)
-        return (
-          <Space>
-            <EyeOutlined
-              onClick={() => viewDetails(record)}
-              style={{ color: 'blue', cursor: 'pointer' }}
-            />
-            <EditOutlined
-              onClick={() => edit(record)}
-              style={{ color: 'green', cursor: 'pointer' }}
-            />
-            <DeleteOutlined
-              onClick={() => handleDelete(record)}
-              style={{ color: 'red', cursor: 'pointer' }}
-            />
-          </Space>
-        )
-      },
+      render: (_, record) => (
+        <>
+          <Button
+            onClick={() => edit(record)}
+            style={{ marginRight: 8 }}
+            icon={<EditOutlined />}
+          />
+          <Button
+            onClick={() => viewDetail(record)}
+            style={{ marginRight: 8 }}
+            icon={<EyeOutlined />}
+          />
+          <Button
+            onClick={() => deleteRecord(record.id)}
+            icon={<DeleteOutlined />}
+          />
+        </>
+      ),
     },
   ]
 
-  // Lọc dữ liệu theo tên
-  const filteredData = gridData.filter(item =>
-    item.name.toLowerCase().includes(searchText.toLowerCase())
-  )
-
-  // Áp dụng sắp xếp cho dữ liệu đã lọc
-  const sortedData = filteredData.slice().sort((a, b) => {
-    const sortOrder = sortedInfo.order === 'descend' ? -1 : 1
-    const sortKey = sortedInfo.columnKey || 'id'
-
-    const canLocaleCompare =
-      a[sortKey] && b[sortKey] && typeof a[sortKey].localeCompare === 'function'
-
-    return canLocaleCompare
-      ? a[sortKey].localeCompare(b[sortKey]) * sortOrder
-      : (a[sortKey] - b[sortKey]) * sortOrder
-  })
-
-  // Áp dụng phân trang cho dữ liệu đã lọc và sắp xếp
-  const start = (pagination.current - 1) * pagination.pageSize
-  const end = start + pagination.pageSize
-  const paginatedData = sortedData.slice(start, end)
-
-  const handleOk = () => {
-    setIsModalVisible(false)
-  }
-
-  const handleCancel = () => {
-    setIsModalVisible(false)
-  }
-
-  const showModal = () => {
-    setIsModalVisible(true)
-  }
-
   return (
     <div className="employeeLayout">
-      <Button type="primary" onClick={showModal} style={{ marginBottom: 16 }}>
-        Create User
-      </Button>
-      <Search
-        placeholder="Tìm kiếm theo tên"
-        onChange={handleChange}
-        style={{ width: 200, marginBottom: 16 }}
-        prefix={<SearchOutlined />}
-      />
-      <Table
-        columns={columns}
-        dataSource={paginatedData.map(item => ({ ...item, key: item.id }))}
-        bordered
-        onChange={handleTableChange}
-        pagination={pagination}
-      />
-      <Modal
-        title="Create User"
-        open={isModalVisible}
-        onOk={handleOk}
-        onCancel={handleCancel}
-      >
-        <Form ref={formRef}>
-          <Form.Item
-            label="Name"
-            name="name"
-            rules={[{ required: true, message: 'Please input name!' }]}
-          >
-            <Input />
-          </Form.Item>
-          {/* Add more form fields as needed */}
-        </Form>
-      </Modal>
+      <Spin spinning={loadingData}>
+        <Button
+          type="primary"
+          onClick={() => setIsModalVisible(true)}
+          style={{ marginBottom: 16 }}
+        >
+          Create
+        </Button>
+        <Search
+          placeholder="Search"
+          onChange={handleChange}
+          style={{ width: 200, marginBottom: 16 }}
+          prefix={<SearchOutlined />}
+        />
+        <Table
+          columns={columns}
+          dataSource={gridData}
+          bordered
+          onChange={handleTableChange}
+          pagination={pagination}
+        />
+        <Modal
+          title="Create"
+          open={isModalVisible}
+          onOk={save}
+          onCancel={() => setIsModalVisible(false)}
+        >
+          <Form ref={formRef}>
+            <Form.Item
+              label="Name"
+              name="name"
+              rules={[{ required: true, message: 'Vui lòng nhập tên!' }]}
+            >
+              <Input />
+            </Form.Item>
+          </Form>
+        </Modal>
+      </Spin>
     </div>
   )
 }
