36639c62be36218f339764cde56db1588e524a76
NguyenCao-GenTech <148932805+CaoVinhNguyen2206@users.noreply.github.com>
2023-12-05 11:39:29 +0700

    [Final -25] Show list employee UI (#8)
    * feat: show list employee UI
    
    * fix: employee action table UI
    
    * fix: table layout UI fetch api
    
    * fix: bug table
    
    * feat: declare component table
    
    * fix: layout table, declare component, css
    
    * fix: bug of sonar
    
    * fix: bug of sonar again
    
    * fix: bug of sonar again 2
    
    * fix: paginate with total
    
    * fix: component table text
    
    * fix: paginate, sort, search, declare component
    
    * fix: translate table
    
    * fix: translate
    
    * fix: table responsive
    
    * fix: sonar clean code
    
    * fix: table responsive 2
    
    * fix: import and change name datatable
    
    * fix: table layout responsive
    
    ---------
    
    Co-authored-by: Hậu-Hoàng <hauhoang@domain.com>

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
+import { Toast } from '@components/Toast/toast.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Suspense fallback={<Spinner />}>
         <App />
+        <Toast />
       </Suspense>
     </BrowserRouter>
   </React.StrictMode>
