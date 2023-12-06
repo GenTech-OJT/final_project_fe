de9166af637b5ef05431daf25cfd5452d3e037eb
nhatnguyen-cr7 <nhat.nguyen-ojt07@devplus.edu.vn>
2023-12-06 11:34:42 +0700

    feat: login

--- src/pages/AdminPages/Login/index.jsx
+++ src/pages/AdminPages/Login/index.jsx
@@ -1,7 +1,136 @@
-import React from 'react'
+import { useNavigate } from 'react-router'
+import { Button, Checkbox, Form, Grid, Input, theme, Typography } from 'antd'
+import { LockOutlined, MailOutlined } from '@ant-design/icons'
+import { showToast } from '@components/Toast/toast'
 
-const Login = () => {
-  return <div>Login</div>
+const { useToken } = theme
+const { useBreakpoint } = Grid
+const { Text, Title } = Typography
+
+export default function App() {
+  const navigate = useNavigate()
+  const { token } = useToken()
+  const screens = useBreakpoint()
+  const onFinish = async values => {
+    console.log('Received values of form: ', values)
+    try {
+      const response = await fetch('http://localhost:3000/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: values.email,
+          password: values.password,
+        }),
+      })
+      const data = await response.json()
+      if (response.ok) {
+        console.log('Login successful:', data)
+        localStorage.setItem('admin', data.token)
+        navigate('/')
+        showToast('đăng nhập thành công', 'success')
+      } else {
+        console.error('Login failed:', data.error)
+        showToast('Đăng nhập thất bại', 'error')
+      }
+    } catch (error) {
+      console.error('An error occurred:', error)
+    }
+  }
+  const styles = {
+    container: {
+      margin: '0 auto',
+      padding: screens.md
+        ? `${token.paddingXL}px`
+        : `${token.sizeXXL}px ${token.padding}px`,
+      width: '380px',
+      border: '1px solid grey',
+      borderRadius: '10px',
+    },
+    footer: {
+      marginTop: token.marginLG,
+      textAlign: 'center',
+      width: '100%',
+    },
+    forgotPassword: {
+      float: 'right',
+    },
+    header: {
+      marginBottom: token.marginXL,
+    },
+    section: {
+      alignItems: 'center',
+      backgroundColor: token.colorBgContainer,
+      display: 'flex',
+      height: screens.sm ? '100vh' : 'auto',
+      padding: screens.md ? `${token.sizeXXL}px 0px` : '20px',
+    },
+    text: {
+      color: token.colorTextSecondary,
+    },
+    title: {
+      fontSize: screens.md ? token.fontSizeHeading2 : token.fontSizeHeading3,
+    },
+  }
+
+  return (
+    <section style={styles.section}>
+      <div style={styles.container}>
+        <div style={styles.header}>
+          <Title style={styles.title}>Sign in</Title>
+          <Text style={styles.text}>
+            Please enter your details below to sign in.
+          </Text>
+        </div>
+        <Form
+          name="normal_login"
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          layout="vertical"
+          requiredMark="optional"
+        >
+          <Form.Item
+            name="email"
+            rules={[
+              {
+                type: 'email',
+                required: true,
+                message: 'Please input your Email!',
+              },
+            ]}
+          >
+            <Input prefix={<MailOutlined />} placeholder="Email" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your Password!',
+              },
+            ]}
+          >
+            <Input.Password
+              prefix={<LockOutlined />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+          </Form.Item>
+          <Form.Item style={{ marginBottom: '0px' }}>
+            <Button block="true" type="primary" htmlType="submit">
+              Log in
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </section>
+  )
 }
-
-export default Login
