be876c2724a09dbe58290794bf80061a4c8d017c
Ngo-Tan-Khoa <khoa.ngo-ojt07@devplus.edu.vn>
2023-12-08 09:25:33 +0700

    fix: change div to grid layout

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import AppHeader from '@components/AdminComponent/AppHeader'
 import AvatarGroup from '@components/AdminComponent/AvatarGroup'
 import PageContent from '@components/AdminComponent/PageContent'
 import SideMenu from '@components/AdminComponent/SideMenu'
-import { Button, Drawer, Layout, theme } from 'antd'
+import { Button, Col, Drawer, Layout, Row, theme } from 'antd'
 import { useEffect, useState } from 'react'
 import { Route, Routes, useNavigate } from 'react-router'
 import './App.css'
@@ -86,40 +86,41 @@ const App = () => {
           >
             {/* <div className="demo-logo-vertical" /> */}
 
-            <div
-              style={{
-                display: 'flex',
-                alignItems: 'center',
-                paddingTop: '11px',
-                justifyContent: 'space-between',
-              }}
+            <Row
+              align="middle"
+              justify="space-between"
+              style={{ paddingTop: '11px' }}
             >
-              {/* <Button
-                className="menu-btn"
-                type="text"
-                icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-                onClick={() => setCollapsed(!collapsed)}
-              /> */}
-              <a
-                href="/"
-                onClick={() => navigate('/')}
-                onKeyDown={() => navigate('/')}
-              >
-                <img
-                  style={{ cursor: 'pointer', marginLeft: '12px' }}
-                  src="https://wieldy.g-axon.work/assets/images/logo-white.png"
-                  alt=""
-                  className="logo"
-                />
-              </a>
-              <button
-                className="close-icon"
-                onClick={onClose}
-                onKeyDown={onClose}
-              >
-                <CloseOutlined />
-              </button>
-            </div>
+              <Col>
+                {/* <Button
+          className="menu-btn"
+          type="text"
+          icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+          onClick={() => setCollapsed(!collapsed)}
+        /> */}
+                <a
+                  href="/"
+                  onClick={() => navigate('/')}
+                  onKeyDown={() => navigate('/')}
+                >
+                  <img
+                    style={{ cursor: 'pointer', marginLeft: '12px' }}
+                    src="https://wieldy.g-axon.work/assets/images/logo-white.png"
+                    alt=""
+                    className="logo"
+                  />
+                </a>
+              </Col>
+              <Col>
+                <Button
+                  className="close-icon"
+                  onClick={onClose}
+                  onKeyDown={onClose}
+                >
+                  <CloseOutlined />
+                </Button>
+              </Col>
+            </Row>
             {/* Avatar */}
             <AvatarGroup
               setIsLogin={setIsLogin}
