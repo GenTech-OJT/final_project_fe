420bd892380d812bf2d8dcd0108c4bb7b8b7854f
toannguyen-valuence <nguyen.minh.toan@valuence.inc>
2023-12-14 01:44:56 +0700

    fix: fix call api edit employ

--- src/apis/employeeApi.js
+++ src/apis/employeeApi.js
@@ -39,3 +39,29 @@ export const createEmployeeApi = async data => {
 
   return response.data
 }
+
+export const getEmployeeByIdApi = async id => {
+  const response = await axios.get(`${API_URL.EMPLOYEES}/${id}`)
+
+  if (response.status !== 200) {
+    throw new Error(`HTTP error! Status: ${response.status}`)
+  }
+
+  return response.data
+}
+
+export const updateEmployeeApi = async (id, data) => {
+  const response = await axios.put(`${API_URL.EMPLOYEES}/${id}`, data, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  })
+
+  if (response.status !== 200) {
+    throw new Error(`HTTP error! Status: ${response.status}`)
+  }
+
+  console.log(response, data)
+
+  return response.data
+}
--- src/hooks/useEmployee.js
+++ src/hooks/useEmployee.js
@@ -1,13 +1,12 @@
 import { useQuery, useMutation } from '@tanstack/react-query'
 import { QUERY_KEY } from '@constants/reactQuery'
-import { getEmployeesApi, createEmployeeApi } from '@api/employeeApi'
-
-// export const useGetEmployees = params => {
-//   return useQuery(['toan', params], async () => {
-//     const { data } = await getEmployeesApi(params)
-//     return data
-//   })
-// }
+import {
+  getEmployeesApi,
+  createEmployeeApi,
+  getEmployeeByIdApi,
+  updateEmployeeApi,
+} from '@api/employeeApi'
+import { useNavigate } from 'react-router-dom'
 
 export const useGetEmployees = params => {
   return useQuery({
@@ -25,106 +24,25 @@ export const useCreateEmployee = () => {
   })
 }
 
-// export const useCreateCard = () => {
-//   const navigate = useNavigate();
+export const useGetEmployeeById = id => {
+  const navigate = useNavigate()
 
-//   return useMutation(
-//     async (params: CardCreateParams) => {
-//       const response = await createCardApi(params);
-//       return response.data;
-//     },
-//     {
-//       onSuccess({ message }) {
-//         notificationSuccess(message);
-//         navigate('/cards');
-//       },
-//       onError({ response }) {
-//         notificationError(response.data.message);
-//       },
-//     },
-//   );
-// };
+  return useQuery({
+    queryKey: [QUERY_KEY.EMPLOYEES, id],
+    queryFn: () => getEmployeeByIdApi(id),
+    onSuccess: (data, variables, context) => {},
+    onError: (error, variables, context) => {
+      navigate('/404')
+    },
+    onSettled: (data, error, variables, context) => {},
+  })
+}
 
-// export const useDeleteCard = () =>
-//   useMutation(
-//     async (id: number) => {
-//       const response = await deleteCardApi(id);
-//       return response.data;
-//     },
-//     {
-//       onSuccess({ message }) {
-//         notificationSuccess(message);
-//       },
-//       onError({ response }) {
-//         notificationError(response.data.message);
-//       },
-//     },
-//   );
-
-// export const useRevertCard = () =>
-//   useMutation(
-//     async (id: number) => {
-//       const response = await revertCardApi(id);
-//       return response.data;
-//     },
-//     {
-//       onSuccess({ message }) {
-//         notificationSuccess(message);
-//       },
-//       onError({ response }) {
-//         notificationError(response.data.message);
-//       },
-//     },
-//   );
-
-// export const useGetCardById = (id: number) => {
-//   const navigate = useNavigate();
-
-//   return useQuery(
-//     [QUERY_KEY.CARDS, id],
-//     async () => {
-//       const { data } = await getCardByIdApi(id);
-//       return data.data;
-//     },
-//     {
-//       onError() {
-//         navigate('/404');
-//       },
-//       retry: false,
-//     },
-//   );
-// };
-
-// export const useUpdateCard = () => {
-//   const navigate = useNavigate();
-//   const queryClient = useQueryClient();
-
-//   return useMutation(
-//     async (params: CardInterface) => {
-//       const response = await updateCardApi(params);
-//       return response.data;
-//     },
-//     {
-//       onSuccess({ message }) {
-//         notificationSuccess(message);
-//         queryClient.refetchQueries([QUERY_KEY.CARDS]);
-//         navigate('/cards');
-//       },
-//       onError({ response }) {
-//         notificationError(response.data.message);
-//       },
-//     },
-//   );
-// };
-
-// export const useGetAllCardsOpenTeacher = () =>
-//   useQuery([QUERY_KEY.CARDS_OPEN], async () => {
-//     const { data } = await getAllCardsOpenTeacherApi();
-//     return data.data;
-//   });
-
-// export const useGetAllStudentCards = () =>
-//   useQuery([QUERY_KEY.STUDENT_CARDS], async () => {
-//     const { data } = await getAllStudentCards();
-//     return data.data;
-//   });
+export const useUpdateEmployee = () => {
+  return useMutation({
+    mutationFn: ({ id, data }) => updateEmployeeApi(id, data),
+    onSuccess: (data, variables, context) => {},
+    onError: (error, variables, context) => {},
+    onSettled: (data, error, variables, context) => {},
+  })
+}
