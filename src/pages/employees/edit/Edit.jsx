762a011dc1b8f240c1f3d66690d438c7e28a86dd
Ngo-Tan-Khoa <khoa.ngo-ojt07@devplus.edu.vn>
2023-12-13 12:22:15 +0700

    fix: sonar

--- src/pages/employees/edit/Edit.jsx
+++ src/pages/employees/edit/Edit.jsx
@@ -20,6 +20,7 @@ import { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router'
 import './Edit.css'
 import { useTranslation } from 'react-i18next'
+import SelectManager from './SelectManager'
 const { Title, Text } = Typography
 
 const EditEmployee = () => {
@@ -116,51 +117,7 @@ const EditEmployee = () => {
         })
     })
   }
-  const SelectManager = () => {
-    const [managers, setManagers] = useState([])
-    const [selectedManager, setSelectedManager] = useState('')
-    const { t } = useTranslation('translation')
 
-    useEffect(() => {
-      fetch('http://localhost:3000/employees')
-        .then(response => response.json())
-        .then(data => {
-          const managerNames = data.data.map(m => m.name)
-          setManagers(managerNames)
-        })
-        .catch(error => {
-          // console.error('Error fetching data:', error)
-        })
-    }, [])
-
-    const handleManagerChange = value => {
-      setSelectedManager(value)
-    }
-
-    return (
-      <Form.Item
-        label={
-          <span style={{ fontWeight: 'bold' }}>{t('employee.manager')}</span>
-        }
-        name="manager"
-        className="select-width-dobgs"
-        rules={[
-          {
-            required: true,
-            message: 'Please select the employee manager !',
-          },
-        ]}
-      >
-        <Select value={selectedManager} onChange={handleManagerChange}>
-          {managers.map(m => (
-            <Select.Option key={m} value={m}>
-              {m}
-            </Select.Option>
-          ))}
-        </Select>
-      </Form.Item>
-    )
-  }
   const checkFile = file => {
     const isImage = file.type.startsWith('image/')
 
