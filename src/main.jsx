44cbdd67f1c297e7cfe68d8a480245073bdd34fa
toannguyen-valuence <nguyen.minh.toan@valuence.inc>
2023-12-13 12:52:11 +0700

    fix: fix staleTime max 5p

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,13 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Toast } from '@components/toast/Toast'
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000,
+    },
+  },
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
