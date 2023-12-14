458b0db7c6a2e36dd3cf24590ef103a95b552f5c
toannguyen-valuence <nguyen.minh.toan@valuence.inc>
2023-12-14 12:44:43 +0700

    fix: fix redirect default page

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,6 +1,7 @@
 import { lazy } from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import PrivateRoute from '@components/PrivateRoute'
+import { useSelector } from 'react-redux'
 const Login = lazy(() => import('@pages/login/Login'))
 const Admin = lazy(() => import('@pages/Admin'))
 const Dashboard = lazy(() => import('@pages/dashboard/Dashboard'))
@@ -11,10 +12,22 @@ const EmployeeDetail = lazy(() => import('@pages/employees/detail/Detail'))
 const NotFoundPage = lazy(() => import('@pages/notFound/NotFound'))
 
 const AppRoutes = () => {
+  const isAuthenticated = useSelector(state => state.auth.isAuthenticated)
+  const isLoggedIn = localStorage.getItem('isLoggedIn')
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="*" element={<NotFoundPage />} />
+      <Route
+        path="/"
+        element={
+          isAuthenticated || isLoggedIn ? (
+            <Navigate to="/admin" />
+          ) : (
+            <Navigate to="/login" />
+          )
+        }
+      />
       <Route path="/admin" element={<PrivateRoute component={<Admin />} />}>
         <Route path="dashboard" element={<Dashboard />} />
         <Route path="projects" element={<Dashboard />} />
