bc9b8a536aabda6df01deba3afa7a81bd78d1841
Nguyen Vo <nguyenvo06022001@gmail.com>
2023-12-14 15:24:30 +0700

    fix: set key for dashboard

--- src/hooks/useDashboard.js
+++ src/hooks/useDashboard.js
@@ -1,10 +1,10 @@
-import { useQuery } from '@tanstack/react-query'
 import { QUERY_KEY } from '@constants/reactQuery'
+import { useQuery } from '@tanstack/react-query'
 import { getDashboardApi } from '../apis/dashboardAPI'
 
 export const useGetDashboard = params => {
   return useQuery({
-    // queryKey: [QUERY_KEY.MANAGERS, params],
+    queryKey: [QUERY_KEY.DASHBOARD, params],
     queryFn: () => getDashboardApi(params),
   })
 }
