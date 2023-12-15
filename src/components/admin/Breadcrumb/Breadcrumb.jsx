fd6a0146802d4e71337d96d2bc5f3dc605892921
VyNguyen-GenTech <148932974+VyNguyen-GenTech@users.noreply.github.com>
2023-12-15 09:26:05 +0700

    [Final-64] Refactor breadcrumb code (#52)
    * fix: refactor breadcrumb code
    
    * fix: fix code smell
    
    ---------
    
    Co-authored-by: kieuvy29 <vy10b3.thptgl@gmail.com>

--- src/components/admin/Breadcrumb/Breadcrumb.jsx
+++ src/components/admin/Breadcrumb/Breadcrumb.jsx
@@ -1,65 +1,29 @@
-import { Breadcrumb } from 'antd'
-import { useTranslation } from 'react-i18next'
-import { Link, useLocation } from 'react-router-dom'
+import { Breadcrumb as BreadcrumbAntd } from 'antd'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
-const BreadCrumb = () => {
-  const { t } = useTranslation('translation')
+const { Item } = BreadcrumbAntd
 
-  const customBreadcrumbNames = {
-    projects: t('breadcrumbs.projects_breadcrumb'),
-    employees: t('breadcrumbs.employees_breadcrumb'),
-    detail: t('breadcrumbs.detail_breadcrumb'),
-    create: t('breadcrumbs.create_breadcrumb'),
-    edit: t('breadcrumbs.edit_breadcrumb'),
-  }
-
-  const location = useLocation()
-  const pathSnippets = location.pathname.split('/').filter(i => i)
-
-  // Find the index of 'detail' or 'edit' in pathSnippets
-  const detailIndex = pathSnippets.indexOf('detail')
-  const editIndex = pathSnippets.indexOf('edit')
-
-  // If 'detail' exists, remove elements after it
-  if (detailIndex !== -1) {
-    pathSnippets.splice(detailIndex + 1)
-  } else if (editIndex !== -1) {
-    // If 'edit' exists, remove elements after it
-    pathSnippets.splice(editIndex + 1)
-  }
-
-  // Kiểm tra xem path có trùng với route không
-  const isPathMatched =
-    pathSnippets.length > 0 &&
-    pathSnippets.every(snippet => customBreadcrumbNames[snippet])
-
-  if (!isPathMatched) {
-    return null // Nếu không trùng, không hiển thị breadcrumb
-  }
-
+const Breadcrumb = ({ items }) => {
   return (
-    <Breadcrumb
-      style={{ margin: '20px 25px 5px', fontWeight: '500', color: '#123ec7' }}
-    >
-      {pathSnippets.map((snippet, index) => {
-        const url = `/${pathSnippets.slice(0, index + 1).join('/')}`
-        const displayText = customBreadcrumbNames[snippet] || snippet
-
-        // Kiểm tra xem đó có phải là mục breadcrumb cuối cùng không
-        const isLastItem = index === pathSnippets.length - 1
-
-        return (
-          <Breadcrumb.Item key={url}>
-            {isLastItem ? (
-              <span>{displayText}</span>
-            ) : (
-              <Link to={url}>{displayText}</Link>
-            )}
-          </Breadcrumb.Item>
-        )
-      })}
-    </Breadcrumb>
+    <BreadcrumbAntd style={{ paddingBottom: 10 }}>
+      {items?.map(({ id, route, title }) => (
+        <Item key={id}>
+          <Link to={route ?? `/${id}`}>{title}</Link>
+        </Item>
+      ))}
+    </BreadcrumbAntd>
   )
 }
 
-export default BreadCrumb
+Breadcrumb.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      key: PropTypes.string.isRequired,
+      route: PropTypes.string,
+      title: PropTypes.string.isRequired,
+    })
+  ),
+}
+
+export default Breadcrumb
