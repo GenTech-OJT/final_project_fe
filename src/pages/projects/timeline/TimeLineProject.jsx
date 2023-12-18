937c9a89594e4be39fec1af5cf9c550d382bd439
Hậu-Hoàng <hauhoang@domain.com>
2023-12-18 21:57:40 +0700

    fix: timeline layout

--- src/pages/projects/timeline/TimeLineProject.jsx
+++ src/pages/projects/timeline/TimeLineProject.jsx
@@ -1,6 +1,11 @@
 /* eslint-disable no-unused-vars */
+import {
+  AppstoreOutlined,
+  MenuUnfoldOutlined,
+  UserOutlined,
+} from '@ant-design/icons'
 import { useGetProjectById } from '@hooks/useProject'
-import { Timeline } from 'antd'
+import { Button, Card, Timeline } from 'antd'
 import moment from 'moment'
 import { useState } from 'react'
 import { useParams } from 'react-router'
@@ -13,6 +18,12 @@ const TimeLineProject = () => {
 
   const [mode, setMode] = useState('alternate')
 
+  const [reverse, setReverse] = useState(false)
+
+  const handleClick = () => {
+    setReverse(!reverse)
+  }
+
   const onChange = e => {
     setMode(e.target.value)
   }
@@ -24,12 +35,15 @@ const TimeLineProject = () => {
           label: project.name,
           children: (
             <div>
-              <p>{`Start Date: ${moment(project.start_date).format(
+              <p
+                style={{ margin: '20px 0', marginLeft: '7px' }}
+              >{`Start Date: ${moment(project.start_date).format(
                 'DD/MM/YYYY'
               )}`}</p>
             </div>
           ),
           date: moment(project.start_date),
+          dot: <AppstoreOutlined style={{ fontSize: '20px', color: 'blue' }} />,
         },
         ...generateEmployeeTimelineItems(project),
         {
@@ -37,42 +51,99 @@ const TimeLineProject = () => {
           label: project.name,
           children: (
             <div>
-              <p>{`End Date: ${moment(project.end_date).format(
+              <p
+                style={{ margin: '20px 0', marginRight: '7px' }}
+              >{`End Date: ${moment(project.end_date).format(
                 'DD/MM/YYYY'
               )}`}</p>
             </div>
           ),
           date: moment(project.end_date),
+          dot: (
+            <AppstoreOutlined style={{ fontSize: '20px', color: 'green' }} />
+          ),
         },
       ]
     : []
 
   return (
-    <div className="timeline-container">
-      <Timeline mode={mode} items={timelineItems}>
+    <Card
+      className="timeline-card"
+      bordered={false}
+      title="Project Timeline"
+      style={{
+        width: '60%',
+        margin: 'auto',
+        marginTop: '20px',
+        borderRadius: '20px',
+        boxShadow: '0 10px 15px rgba(0, 0, 0, 0.1)',
+        padding: '16px',
+      }}
+    >
+      <Timeline
+        pending="Recording..."
+        reverse={reverse}
+        mode={mode}
+        items={timelineItems}
+      >
         {/* Timeline receives items from timelineItems */}
       </Timeline>
-    </div>
+
+      <Button
+        type="primary"
+        style={{
+          marginTop: 16,
+          fontWeight: 'bold',
+          width: '50%',
+          marginLeft: '27%',
+        }}
+        onClick={handleClick}
+      >
+        {<MenuUnfoldOutlined />} REVERSE
+      </Button>
+    </Card>
   )
 }
 
 const generateEmployeeTimelineItems = project => {
+  const getRandomColor = () =>
+    `#${Math.floor(Math.random() * 16777215).toString(16)}`
   return project.employees
     ? project.employees.flatMap(employee => {
+        const employeeIcon = (
+          <UserOutlined style={{ fontSize: '20px', color: getRandomColor() }} />
+        )
         return employee.periods
           ? employee.periods.map(period => ({
               key: `${employee.id}-${period.joining_time}`,
               label: `${getEmployeeName(employee.id)}`,
               children: (
-                <div>
-                  <p>{`(${moment(period.joining_time).format(
-                    'DD/MM/YYYY'
-                  )} - ${moment(period.leaving_time || project.end_date).format(
-                    'DD/MM/YYYY'
-                  )})`}</p>
-                </div>
+                <Card
+                  style={{
+                    height: '10%',
+                    width: '90%',
+                    margin: 'auto',
+                    marginTop: '20px',
+                    borderRadius: '20px',
+                    boxShadow: '0 10px 15px rgba(0, 0, 0, 0.1)',
+                  }}
+                >
+                  <div>
+                    <p style={{ margin: '7px 0' }}>
+                      {`Joining Time: ${moment(period.joining_time).format(
+                        'DD/MM/YYYY'
+                      )}`}
+                    </p>
+                    <p style={{ margin: '7px 0' }}>
+                      {`Leaving Time: ${moment(
+                        period.leaving_time || project.end_date
+                      ).format('DD/MM/YYYY')}`}
+                    </p>
+                  </div>
+                </Card>
               ),
               date: moment(period.joining_time),
+              dot: employeeIcon,
             }))
           : []
       })
