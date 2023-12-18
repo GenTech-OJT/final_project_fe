0f416845df32ef90ae2c88f30fef7b746f8ad205
Nguyen Vo <nguyenvo06022001@gmail.com>
2023-12-18 10:48:43 +0700

    fix: layout api dashboard

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -11,6 +11,7 @@ import { useGetDashboard } from '../../hooks/useDashboard'
 import { ChartBar, ChartPie } from './Chart/Chart'
 import './Dashboard.css'
 
+const { Meta } = Card
 const Dashboard = () => {
   const { data, isLoading } = useGetDashboard()
 
@@ -33,60 +34,75 @@ const Dashboard = () => {
       <BreadCrumb items={breadcrumbItems} />
       <Row gutter={{ xs: 8, sm: 12, md: 16, lg: 24 }}>
         <Col xs={24} sm={12} md={6}>
-          <Card>
+          <Card style={{ backgroundColor: '#f0f0f0' }}>
             <Flex justify="space-between">
               <Statistic
                 title={t('dashboard_page.employees')}
                 value={data?.employeeCount}
                 className="custom-statistic"
               ></Statistic>
-              <UserOutlined className="custom-icon" />
+              <UserOutlined className="custom-icon" style={{ color: '#555' }} />
             </Flex>
           </Card>
         </Col>
         <Col xs={24} sm={12} md={6}>
-          <Card>
+          <Card style={{ backgroundColor: '#f0f0f0' }}>
             <Flex justify="space-between">
               <Statistic
                 title={t('dashboard_page.project')}
                 value={data?.projectCount}
                 className="custom-statistic"
               ></Statistic>
-              <ProjectOutlined className="custom-icon" />
+              <ProjectOutlined
+                className="custom-icon"
+                style={{ color: '#555' }}
+              />
             </Flex>
           </Card>
         </Col>
         <Col xs={24} sm={12} md={6}>
-          <Card>
+          <Card style={{ backgroundColor: '#f0f0f0' }}>
             <Flex justify="space-between">
               <Statistic
                 title={t('dashboard_page.skills')}
                 value={data?.skillsArray.length}
                 className="custom-statistic"
               ></Statistic>
-              <ToolOutlined className="custom-icon" />
+              <ToolOutlined className="custom-icon" style={{ color: '#555' }} />
             </Flex>
           </Card>
         </Col>
         <Col xs={24} sm={12} md={6}>
-          <Card>
+          <Card style={{ backgroundColor: '#f0f0f0' }}>
             <Flex justify="space-between">
               <Statistic
                 title={t('dashboard_page.position')}
                 value={data?.positionCount}
                 className="custom-statistic"
               ></Statistic>
-              <TeamOutlined className="custom-icon" />
+              <TeamOutlined className="custom-icon" style={{ color: '#555' }} />
             </Flex>
           </Card>
         </Col>
       </Row>
       <Row>
-        <Col xs={24} sm={12} md={12}>
-          <ChartPie data={data?.skillsArray} />
+        <Col xs={24} sm={12} md={12} style={{ padding: '10px' }}>
+          <Card>
+            <ChartPie data={data?.skillsArray} />
+            <Meta
+              title="Pie Chart about Skills of employees"
+              style={{ textAlign: 'center' }}
+            ></Meta>
+          </Card>
         </Col>
-        <Col xs={24} sm={12} md={12}>
-          <ChartBar />
+        <Col xs={24} sm={12} md={12} style={{ padding: '10px' }}>
+          <Card>
+            <ChartBar />
+            <Meta
+              title="Bar Chart about Projects in stages every 2 months"
+              style={{ textAlign: 'center' }}
+            ></Meta>
+          </Card>
         </Col>
       </Row>
     </div>
