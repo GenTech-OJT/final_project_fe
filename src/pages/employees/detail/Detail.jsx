7308360b2b19389daacb07c65c835d81e4e456fc
kieuvy29 <vy10b3.thptgl@gmail.com>
2023-12-19 14:31:43 +0700

    fix: done employee detail

--- src/pages/employees/detail/Detail.jsx
+++ src/pages/employees/detail/Detail.jsx
@@ -4,16 +4,16 @@ import {
   Col,
   Descriptions,
   Flex,
-  List,
   Row,
   Space,
+  Spin,
   Tabs,
   Tag,
   Tooltip,
   Typography,
 } from 'antd'
 import Title from 'antd/es/skeleton/Title'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
 import { CalendarOutlined, EyeOutlined } from '@ant-design/icons'
@@ -39,28 +39,6 @@ const EmployeeDetail = () => {
     useGetProjectsByEmployeeId(id, searchText)
 
   const { t } = useTranslation('translation')
-
-  const [activeTab, setActiveTab] = useState('1') // State to keep track of active tab
-
-  useEffect(() => {
-    const storedActiveTab = localStorage.getItem('activeTab')
-    if (storedActiveTab) {
-      setActiveTab(storedActiveTab)
-    }
-  }, [])
-
-  const handleTabChange = (key, e) => {
-    e.preventDefault()
-    setActiveTab(key)
-    // Store the active tab in localStorage
-    localStorage.setItem('activeTab', key)
-  }
-
-  const handleChange = e => {
-    e.preventDefault()
-    const value = e.target.value
-    setSearchText(value)
-  }
 
   const getStatusDotColor = () => {
     return employee_details.status === 'active' ? 'green' : 'red'
@@ -93,7 +71,7 @@ const EmployeeDetail = () => {
   }
 
   if (isLoading || !employee_details || isLoadingProject) {
-    return <div>Loading...</div>
+    return <Spin spinning={true} fullscreen />
   }
   // Function to get vibrant colors
   const getTagColor = index => {
@@ -118,8 +96,6 @@ const EmployeeDetail = () => {
       <Title className="page-title">EMPLOYEE DETAIL</Title>
 
       <Tabs
-        activeKey={activeTab}
-        onChange={handleTabChange}
         defaultActiveKey="1"
         tabPosition="top"
         animated={true}
@@ -282,7 +258,8 @@ const EmployeeDetail = () => {
                           className="custom-label"
                         >
                           {capitalizeFirstLetter(
-                            displayValue(employee_details.description)
+                            displayValue(employee_details.description) ||
+                              t('employee_details.no_description')
                           )}
                         </Descriptions.Item>
 
@@ -303,7 +280,7 @@ const EmployeeDetail = () => {
                               style={{ padding: '5px' }}
                             >
                               {capitalizeFirstLetter(skill.name)} - {skill.year}{' '}
-                              years
+                              {t('employee_details.years')}
                             </Tag>
                           ))}
                         </Descriptions.Item>
@@ -318,72 +295,122 @@ const EmployeeDetail = () => {
         {/* next page */}
 
         <TabPane tab={<span>{t('employee_details.project')}</span>} key="3">
-          <CustomSearch handleChange={handleChange} />
+          <Row>
+            <CustomSearch />
+          </Row>
           <Row gutter={[16, 16]}>
-            {dataProject.map((item, index) => (
-              <Col xs={24} sm={12} md={8} lg={6} key={index}>
-                <Card
-                  actions={[
-                    <Avatar.Group
-                      maxCount={2}
-                      key="avatar"
-                      style={{ cursor: 'default' }}
-                    >
-                      {item.employees.map(employee => (
-                        <Tooltip title={employee.name} key={employee.id}>
-                          <Avatar src={employee.avatar} />
-                        </Tooltip>
-                      ))}
-                    </Avatar.Group>,
-                    <span key={'date'} className="pro_date">
-                      <CalendarOutlined />{' '}
-                      {moment(item.start_date).format('YYYY-MM-DD')}
-                    </span>,
-                    // <span key="date">{item.start_date}</span>,
-                  ]}
-                >
-                  <Row gutter={[16, 16]}>
-                    <Col span={24}>
-                      <Space
-                        size="large"
-                        style={{
-                          width: '100%',
-                          justifyContent: 'space-between',
-                        }}
+            {dataProject.length > 0 ? (
+              dataProject.map((item, index) => (
+                <Col xs={24} sm={12} md={8} lg={6} key={index}>
+                  <Card
+                    actions={[
+                      <Avatar.Group
+                        maxCount={2}
+                        key="avatar"
+                        style={{ cursor: 'default' }}
                       >
-                        <Tag color="error">{item.currentEmployee.position}</Tag>
-                        {/* <Typography.Text ellipsis>...</Typography.Text> */}
-                        <a href="#" className="pro_eye">
-                          <EyeOutlined />
-                        </a>
-                      </Space>
-                    </Col>
-                    <Col span={24}>
-                      <Typography.Title level={4} className="pro_title">
-                        {item.name}
-                      </Typography.Title>
-                      {item.status === 'Pending' && (
-                        <Tag color="#faad14">Pending</Tag>
-                      )}
-                      {item.status === 'In Progress' && (
-                        <Tag color="#1677ff">In Progress</Tag>
-                      )}
-                      {item.status === 'Cancelled' && (
-                        <Tag color="#ff4d4f">Cancelled</Tag>
-                      )}
-                      {item.status === 'Done' && (
-                        <Tag color="#52c41a">Done</Tag>
-                      )}
-                    </Col>
+                        {item.employees.map(employee => (
+                          <Tooltip title={employee.name} key={employee.id}>
+                            <Avatar src={employee.avatar} />
+                          </Tooltip>
+                        ))}
+                      </Avatar.Group>,
+                      <span key={'date'} className="pro_date">
+                        <CalendarOutlined />{' '}
+                        {moment(item.start_date).format('YYYY-MM-DD')}
+                      </span>,
+                      // <span key="date">{item.start_date}</span>,
+                    ]}
+                  >
+                    <Row gutter={[16, 16]}>
+                      <Col span={24}>
+                        <Space
+                          size="large"
+                          style={{
+                            width: '100%',
+                            justifyContent: 'space-between',
+                          }}
+                        >
+                          <Tag color="error">
+                            {item.currentEmployee.position}
+                          </Tag>
+                          <a
+                            href={`/admin/projects/detail/${item.id}`}
+                            className="pro_eye"
+                          >
+                            <EyeOutlined />
+                          </a>
+                        </Space>
+                      </Col>
+                      <Col span={24}>
+                        <Typography.Title level={4} className="pro_title">
+                          {item.name}
+                        </Typography.Title>
+                        {item.status === 'Pending' && (
+                          <Tag color="#faad14">Pending</Tag>
+                        )}
+                        {item.status === 'In Progress' && (
+                          <Tag color="#1677ff">In Progress</Tag>
+                        )}
+                        {item.status === 'Cancelled' && (
+                          <Tag color="#ff4d4f">Cancelled</Tag>
+                        )}
+                        {item.status === 'Done' && (
+                          <Tag color="#52c41a">Done</Tag>
+                        )}
+                      </Col>
 
-                    <Col span={24} className="pro_description">
-                      {item.description}
-                    </Col>
-                    <Col span={24}></Col>
-                  </Row>
-                </Card>
+                      <Col span={24} className="pro_description">
+                        {item.description}
+                      </Col>
+                      <Col span={24}></Col>
+                    </Row>
+                  </Card>
+                </Col>
+              ))
+            ) : (
+              <Col span={24}>
+                <div
+                  style={{
+                    textAlign: 'center',
+                    color: 'gray',
+                    marginTop: '10%',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                  }}
+                >
+                  <svg
+                    width="84"
+                    height="61"
+                    viewBox="0 0 64 41"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <g
+                      transform="translate(0 1)"
+                      fill="none"
+                      fillRule="evenodd"
+                    >
+                      <ellipse
+                        fill="#f5f5f5"
+                        cx="32"
+                        cy="33"
+                        rx="32"
+                        ry="7"
+                      ></ellipse>
+                      <g fillRule="nonzero" stroke="#d9d9d9">
+                        <path d="M55 12.76L44.854 1.258C44.367.474 43.656 0 42.907 0H21.093c-.749 0-1.46.474-1.947 1.257L9 12.761V22h46v-9.24z"></path>
+                        <path
+                          d="M41.613 15.931c0-1.605.994-2.93 2.227-2.931H55v18.137C55 33.26 53.68 35 52.05 35h-40.1C10.32 35 9 33.259 9 31.137V13h11.16c1.233 0 2.227 1.323 2.227 2.928v.022c0 1.605 1.005 2.901 2.237 2.901h14.752c1.232 0 2.237-1.308 2.237-2.913v-.007z"
+                          fill="#fafafa"
+                        ></path>
+                      </g>
+                    </g>
+                  </svg>
+                  {t('employee_details.no_projects')}
+                </div>
               </Col>
-            ))}
+            )}
           </Row>
         </TabPane>
       </Tabs>
