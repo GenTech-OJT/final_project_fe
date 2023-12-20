c9474a03918389888ed9e57ee4773771b06714be
kieuvy29 <vy10b3.thptgl@gmail.com>
2023-12-20 15:53:12 +0700

    fix: fix code sidebar

--- src/components/admin/Breadcrumb/Breadcrumb.jsx
+++ src/components/admin/Breadcrumb/Breadcrumb.jsx
@@ -1,10 +1,14 @@
 import { Breadcrumb as BreadcrumbAntd } from 'antd'
 import PropTypes from 'prop-types'
+import { useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { setSelectedKey } from '../../../redux/Slice/menuSlice'
 
 const { Item } = BreadcrumbAntd
 
 const Breadcrumb = ({ items }) => {
+  const dispatch = useDispatch()
+
   return (
     <BreadcrumbAntd style={{ paddingBottom: 10 }}>
       {items?.map(({ id, route, title }, index) => (
@@ -12,7 +16,14 @@ const Breadcrumb = ({ items }) => {
           {index === items.length - 1 ? (
             <span>{title}</span>
           ) : (
-            <Link to={route ?? `/${id}`}>{title}</Link>
+            <Link
+              to={route ?? `/${id}`}
+              onClick={() => {
+                dispatch(setSelectedKey(route ?? `/${id}`))
+              }}
+            >
+              {title}
+            </Link>
           )}
         </Item>
       ))}
